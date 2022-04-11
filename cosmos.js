f79518884427a19d87d11246f5fc94b56e8b9e0f
mathocihara <matheusyukio.hocihara@gmail.com>
2022-04-11 14:46:44 -0300

    construção do JavaScript

---
+++ cosmos.js
@@ -0,0 +1,10 @@
+var dataEscolhida = document.getElementsByClassName('dataDoUser')
+var nomeEscolhido = document.getElementsByClassName('nomeDoUser')
+
+
+function EnviarData(data, nome){
+    console.log(data)
+    console.log(nome)
+}
+
+EnviarData(dataEscolhida, nomeEscolhido)
